502a67fe45c61f5ed1c02408f4c45117e050068e
Alex <xirurg97@yandex.ru>
2025-06-26 22:27:51 +0300

    добавил проверку добавляемого

--- electromart-api/app/Http/Controllers/ProductController.php
+++ electromart-api/app/Http/Controllers/ProductController.php
@@ -25,14 +25,24 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $validator = validator::make($request->all(), [ #Созадли переменную валидатор, сделали запрос на все и создали необходимые условия
             'title' => 'required|string|max:255',
             'price' => 'required|numeric|min:0',
-            'description' => 'nullable|string',
+            'description' => 'required|string|min:5',
         ]);
 
-        $product = Product::create($validated);
-        return response()->json($product, 201); #Создаем новый товар
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $product = Product::create($validator->validated());
+        return response()->json([
+            'status' =>'success',
+            'product' => $product,
+        ], 201); #Создаем новый товар
     }
 
     public function destroy($id)
